8d10ea024bc4565311a3c8394cd424fa72de4d48
Tosin <tosin@sourcebreaker.com>
2020-05-04 16:30:33 +0100

    WIP:Feature function 90% complete

--- router.js
+++ router.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 const usersController = require('./controllers/usersController');
 const postController = require('./controllers/postController')
+const followController = require('./controllers/followController')
 
 router.get('/', usersController.home);
 router.post('/register', usersController.register);
@@ -11,10 +12,13 @@ router.post('/logout', usersController.logout);
 router.get('/create-post', usersController.mustBeLoggedIn, postController.viewCreateScreen);
 router.post('/create-post', usersController.mustBeLoggedIn, postController.create)
 router.get('/post/:id', postController.viewSingle)
-router.get('/profile/:username', usersController.ifUserExist, usersController.profilePostScreen)
+router.get('/profile/:username', usersController.ifUserExist, usersController.sharedProfileData, usersController.profilePostScreen)
+router.get('/profile/:username/followers', usersController.ifUserExist, usersController.sharedProfileData, usersController.followersProfileScreen)
+router.get('/profile/:username/following', usersController.ifUserExist, usersController.sharedProfileData, usersController.followingProfileScreen)
 router.get('/post/:id/edit', usersController.mustBeLoggedIn, postController.viewEditScreen)
 router.post('/post/:id/edit', usersController.mustBeLoggedIn, postController.edit)
 router.post('/post/:id/delete', usersController.mustBeLoggedIn, postController.delete)
 router.post('/search', usersController.mustBeLoggedIn, postController.search)
-
+router.post('/addFollow/:username', usersController.mustBeLoggedIn, followController.addFollow)
+router.post('/removeFollow/:username', usersController.mustBeLoggedIn, followController.removeFollow)
 module.exports = router;
